4bcfc53708039772ed2da4db75cf6384135cfbdb
Taner Guven <tanerguven@gmail.com>
2015-08-18 23:21:01 +0300

    -

--- virtualization/kvm_fsimg.sh
+++ virtualization/kvm_fsimg.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-FS_FILE=$1
-MEMORY=$2
-KERNEL_PARAMETERS=$3
+if [ "$KVM_MEMORY" == "" ]; then
+	KVM_MEMORY="512"
+fi
 
-MOUNT_DIR=${FS_FILE}.kvm-mount
+
+INPUT_FS_FILE=$1
+if [ "INPUT_FS_FILE" != "" ]; then
+	KVM_FS_FILE=$INPUT_FS_FILE
+fi
+
+if [ "$KVM_FS_FILE" == "" ]; then
+	echo "KVM_FS_FILE empty"
+	exit 1
+fi
+
+MOUNT_DIR=${KVM_FS_FILE}.kvm-mount
 if [ -e "$MOUNT_DIR" ]; then
 	echo "$MOUNT_DIR dir exists"
 	exit 1
@@ -18,16 +29,31 @@ cleanup() {
 }
 
 run() {
-	mkdir $MOUNT_DIR
-	mount -o ro $FS_FILE $MOUNT_DIR
 
-	if [ "" != "$(readlink -f $MOUNT_DIR/boot/vmlinuz-*)" ]; then
+	mkdir $MOUNT_DIR
+
+	mount -o rw $KVM_FS_FILE $MOUNT_DIR || exit
+	eval $EVAL_KVM_COPY_FILES
+	for i in "${KVM_COPY_FILES[@]}" ; do
+		IFS=':' read -r src dst <<< "$i"
+		echo "cp $src $MOUNT_DIR/$dst"
+		cp -f $src $MOUNT_DIR/$dst || exit
+	done
+	umount $MOUNT_DIR
+
+
+	mount -o ro $KVM_FS_FILE $MOUNT_DIR  || exit
+	if [ "" != "$KVM_VMLINUZ" ];then
+		vmlinuz=$KVM_VMLINUZ
+	elif [ "" != "$(readlink -f $MOUNT_DIR/boot/vmlinuz-*)" ]; then
 		vmlinuz=$(readlink -f $MOUNT_DIR/boot/vmlinuz-* | head -n 1)
 	else
 		vmlinuz="/boot/vmlinuz-$(uname -r)"
 	fi
 
-	if [ "" != "$(readlink -f $MOUNT_DIR/boot/initrd.img-*)" ]; then
+	if [ "" != "$KVM_INITRD" ]; then
+		initrd=$KVM_INITRD
+	elif [ "" != "$(readlink -f $MOUNT_DIR/boot/initrd.img-*)" ]; then
 		initrd=$(readlink -f $MOUNT_DIR/boot/initrd.img-* | head -n 1)
 	else
 		initrd="/boot/initrd.img-$(uname -r)"
@@ -37,19 +63,21 @@ run() {
 	echo "initrd: $initrd"
 
 	kvm \
-		-smp $(nproc) -m $MEMORY \
+		-smp $(nproc) -m $KVM_MEMORY \
 		-serial mon:stdio \
 		-kernel "$vmlinuz" \
 		-initrd "$initrd" \
-		-drive file=$FS_FILE,if=virtio \
-		-append "$KERNEL_PARAMETERS root=/dev/vda rw"
+		-drive file=$KVM_FS_FILE,if=virtio \
+		$KVM_PARAMETERS \
+		-append "$KVM_KERNEL_PARAMETERS root=/dev/vda rw"
 }
 
 trap cleanup EXIT
 
-echo "FS_FILE: $FS_FILE"
-echo "KERNEL_PARAMETERS: '$KERNEL_PARAMETERS'"
+echo "KVM_FS_FILE: $KVM_FS_FILE"
+echo "KERNEL_PARAMETERS: '$KVM_KERNEL_PARAMETERS'"
 echo "CPU: $(nproc)"
-echo "MEMORY: $MEMORY"
+echo "MEMORY: $KVM_MEMORY"
+echo "KVM_PARAMETERS: $KVM_PARAMETERS"
 
 run
