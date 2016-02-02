1c514f035f271e83c1343c1089f61c4a08c9426d
Taner Guven <tanerguven@gmail.com>
2016-02-02 22:16:00 +0200

    -

---
+++ installed_files_diff.py
@@ -0,0 +1,32 @@
+import os, sys
+
+if len(sys.argv) < 2:
+    print "usage: %s GIT_DIR" % sys.argv[0]
+    exit(1)
+
+GIT_DIR=os.path.abspath(sys.argv[1])
+
+FILE_LIST={}
+
+for f in (
+        "cp2", "cprsync", "ddwatch", "em", "firefox", "fsimg", "randomgen", "randompass",
+        "run_with_aufs_ram_cache", "run_with_ram_cache", "tmpclean.sh", "tmpfs_compressed.py",
+        "tmpmnt_archivemount", "umountlist", "xclips"):
+    FILE_LIST["/usr/local/bin/%s" % f] = "scripts/bin/%s" % f
+
+
+for f in ("kvm_shared_fs.sh", "kvm_aufs_root.sh", "kvm_fsimg.sh"):
+    FILE_LIST["/usr/local/bin/%s" % f] = "scripts/virtualization/%s" % f
+
+FILE_LIST["/usr/local/bin/run_docker.sh"] = "containers/docker/scripts/run_docker.sh"
+
+if __name__ == '__main__':
+    for k,v in FILE_LIST.iteritems():
+        v = "%s/%s" % (GIT_DIR, v)
+        if not os.path.isfile(k):
+            print "[NOT_INSTALLED] %s -> %s" % (k, v)
+        elif not os.path.isfile(v):
+            print "[NOT_FOUND] %s -> %s" % (k, v)
+        elif open(k).read() != open(v).read():
+            print "[DIFFERENT] %s -> %s" % (k, v)
+
