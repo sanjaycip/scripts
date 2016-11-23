1914a45cfddc458901cc8e408854ed55832d9770
Taner Guven <tanerguven@gmail.com>
2016-11-23 16:44:29 +0300

    ssh-vpn baglanti kopunca otomatik baglanma

--- bin/ssh-vpn.sh
+++ bin/ssh-vpn.sh
@@ -9,6 +9,7 @@ if [ "$1" == "--help" ] || [ "$1" == "" ]; then
 fi
 
 #env SERVER_SCRIPT=local|remote|path
+#env SSH_PARAMETERS=
 
 if [ "$SERVER_SCRIPT" == "" ]; then
 	SERVER_SCRIPT=local
@@ -16,8 +17,9 @@ fi
 
 ID_RSA=$1
 SERVER_IP_OR_DOMAIN=$2
-TUN_NO=$3
-SERVER_PORT=$4
+SERVER_PORT=$3
+
+TUN_NO=0 # hata verirse otomatik arttiriyor
 
 DNS=8.8.8.8
 
@@ -29,8 +31,16 @@ validate_ip() {
 	fi
 }
 
-# FIXME: get_gateway ve get_interface fonksiyonlarina duzgun bir cozum bulunmali
 get_gateway() {
+	if is_android; then
+		get_gateway_android
+	else
+		get_gateway_pc
+	fi
+}
+
+# FIXME: get_gateway_pc ve get_interface_pc fonksiyonlarina duzgun bir cozum bulunmali
+get_gateway_pc() {
 	a=`ip route get 8.8.8.8 | grep via | wc -l`
 	if [ "$a" == "0" ]; then
 		ip route get 8.8.8.8 | head -n 1 | awk '{print $(NF-0)}' | tr -d '\n'
@@ -39,8 +49,61 @@ get_gateway() {
 	fi
 }
 
+calculate_gateway() {
+	IP=$1
+	PREFIX=$2
+
+	IFS=. read -r i1 i2 i3 i4 <<< $IP
+	IFS=. read -r xx m1 m2 m3 m4 <<< $(for a in $(seq 1 32); do if [ $(((a - 1) % 8)) -eq 0 ]; then echo -n .; fi; if [ $a -le $PREFIX ]; then echo -n 1; else echo -n 0; fi; done)
+	a=$(( i1 & (2#$m1) ))
+	b=$(( i2 & (2#$m2) ))
+	c=$(( i3 & (2#$m3) ))
+	d=$(( i4 & (2#$m4) ))
+	d=$((d+1))
+	printf "%d.%d.%d.%d\n" "$a" "$b" "$c" "$d"
+}
+
+get_interface_type_android() {
+	a=`get_interface_android`
+	echo ${a:0:-1}
+}
+
+get_gateway_android() {
+	line=`netcfg | grep UP | grep wlan | head -n 1`
+	if [ "$line" == "" ]; then
+		line=`netcfg | grep UP | grep -v 127.0.0.1 | head -n 1`
+	fi
+	ip_prefix=`echo $line | awk '{print $3}'`
+	IP=`echo $ip_prefix | awk -F '/' '{print $1}'`
+	PREFIX=`echo $ip_prefix | awk -F '/' '{print $2}'`
+
+	if [ "$(get_interface_type_android)" == "wlan" ]; then
+		gateway=`calculate_gateway $IP $PREFIX`
+	else
+		gateway=$IP
+	fi
+
+	echo $gateway
+}
+
 get_interface() {
+	if is_android; then
+		get_interface_android
+	else
+		get_interface_pc
+	fi
+}
+
+get_interface_pc() {
 	ip route get 8.8.8.8 | head -n 1 | awk '{print $5}' | tr -d '\n'
+}
+
+get_interface_android() {
+	line=`netcfg | grep UP | grep wlan | head -n 1`
+	if [ "$line" == "" ]; then
+		line=`netcfg | grep UP | grep -v 127.0.0.1 | head -n 1`
+	fi
+	echo $line | awk '{print $1}'
 }
 
 convert_domain_to_ip() {
@@ -58,12 +121,21 @@ convert_domain_to_ip() {
 
 is_android() {
 	if [ -e "/system/bin/adb" ]; then
-		return 0
+		return 0 # 0 true
 	else
 		return 1
 	fi
 }
 
+connection_error() {
+	IP=$1
+	PORT=$2
+	if nc -w 10 -z $IP $PORT &> /dev/null; then
+		return 1 # 1 false
+	else
+		return 0 # true
+	fi
+}
 clean_exit() {
 	answer=""
 	while [ "$answer" != "exit" ]; do
@@ -75,7 +147,7 @@ clean_exit() {
 	if is_android ; then
 		ip route del table 100 default via $SERVER_TUN_IP
 		ip route del table 100 $SERVER_TUN_IP
-		ip route del table 100 $SERVER_IP via $OLD_GATEWAY
+		ip route del table 100 $SERVER_IP
 		ip route del table 100 default via 127.0.0.1
 		ip rule del table 100
 	else
@@ -86,13 +158,8 @@ clean_exit() {
 	echo "[CLIENT] clean ok"
 }
 
-run() {
-
+config() {
 	[ "$ID_RSA" != "" ] || exit 1
-
-	if [ "$TUN_NO" == "" ]; then
-		TUN_NO=1
-	fi
 
 	if [ "$SERVER_PORT" == "" ]; then
 		SERVER_PORT=22
@@ -111,22 +178,29 @@ run() {
 		fi
 	fi
 
-
-	# tunnel device IP configuration
-	IP_3=$((200 + $TUN_NO))
-	CLIENT_TUN=$TUN_NO
-	CLIENT_TUN_IP=192.168.${IP_3}.2
-	SERVER_TUN=$TUN_NO
-	SERVER_TUN_IP=192.168.${IP_3}.1
-	NETMASK=255.255.255.0
+	trap "clean_exit" SIGTERM EXIT SIGKILL
 
 	OLD_GATEWAY=$(get_gateway)
 	OLD_INTERFACE=$(get_interface)
 	echo "[CLIENT] ssh-server: $SERVER_IP:$SERVER_PORT"
 	echo "[CLIENT] gateway: $OLD_GATEWAY - interface: $OLD_INTERFACE "
 
+	if is_android ; then
+		ip route add $SERVER_IP via $OLD_GATEWAY;
+		ip rule add prio 100 from all lookup 100
+	else # PC
+		ip route add $SERVER_IP via $OLD_GATEWAY;
+	fi
+}
 
-	trap "clean_exit" SIGTERM EXIT SIGKILL
+connect() {
+	# tunnel device IP configuration
+	IP_3=$((10 + $TUN_NO))
+	CLIENT_TUN=$TUN_NO
+	CLIENT_TUN_IP=192.168.${IP_3}.2
+	SERVER_TUN=$TUN_NO
+	SERVER_TUN_IP=192.168.${IP_3}.1
+	NETMASK=255.255.255.0
 
 	if is_android ; then
 		echo "[CLIENT] Android System"
@@ -136,14 +210,18 @@ run() {
 			ln -s /dev/tun /dev/net/tun
 		fi
 
-		ip route add $SERVER_IP via $OLD_GATEWAY;
-		ip rule add prio 100 from all lookup 100
-		ip route add table 100 $SERVER_IP via $OLD_GATEWAY
+		gw=$(get_gateway)
+		interface=$(get_interface)
+		if [ "$gw" == "" ] ; then
+			exit 1
+		fi
+
+		ip route replace table 100 $SERVER_IP via $gw dev $interface
 
 		ROUTE_CONFIG='\
 			ifconfig tun'$CLIENT_TUN' '$CLIENT_TUN_IP' pointopoint '$SERVER_TUN_IP' netmask '$NETMASK'; \
-			ip route add table 100 '$SERVER_TUN_IP' dev tun'$CLIENT_TUN'; \
-			ip route add table 100 default via '$SERVER_TUN_IP';'
+			ip route replace table 100 '$SERVER_TUN_IP' dev tun'$CLIENT_TUN'; \
+			ip route replace table 100 default via '$SERVER_TUN_IP';'
 
 		DNS_CONFIG='\
 			iptables -t nat -I OUTPUT -p tcp --dport 53 -j DNAT --to '$DNS':53 ; \
@@ -157,14 +235,12 @@ run() {
 			setprop dhcp.wlan0.dns4 '$DNS' ;'
 
 	else # PC
-		ip route add $SERVER_IP via $OLD_GATEWAY;
 		ROUTE_CONFIG='\
 			ifconfig tun'$CLIENT_TUN' '$CLIENT_TUN_IP' pointopoint '$SERVER_TUN_IP' netmask '$NETMASK'; \
 			ip route replace default via '$SERVER_TUN_IP';'
 
 		DNS_CONFIG='echo nameserver '$DNS' > /etc/resolv.conf ;'
 	fi
-
 
 	LOCAL_COMMAND='\
 		'$ROUTE_CONFIG' \
@@ -200,9 +276,10 @@ run() {
 	fi
 
 	$SSH_COMMAND \
+		$SSH_PARAMETERS \
 		-o PermitLocalCommand=yes \
 		-o LocalCommand="$LOCAL_COMMAND" \
-		-o ServerAliveInterval=60 \
+		-o ServerAliveInterval=10 -o ServerAliveCountMax=1 \
 		-w $CLIENT_TUN:$SERVER_TUN \
 		\
 		root@$SERVER_IP -p $SERVER_PORT \
@@ -224,4 +301,12 @@ run() {
 	wait
 }
 
-run
+config
+
+while true; do
+	((TUN_NO=(TUN_NO%200)+1))
+	echo "TUN_NO: $TUN_NO"
+
+	connect
+	sleep 5
+done
